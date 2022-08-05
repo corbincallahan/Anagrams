abbbac4810c366cfcd6969904212897dd9b64096
Thomas Marks <tmarks@scitec.com>
2022-08-05 17:01:21 -0600

    Format this to be pep8 compliant

--- Anagrams.py
+++ Anagrams.py
@@ -5,16 +5,20 @@ import time
 
 random.seed(str(date.today()))
 
+
 def scramble(word):
     word = list(word)
     random.shuffle(word)
     return ''.join(word)
 
+
 with open("Dictionary.txt", "r") as dictFile:
     words = [line.strip() for line in dictFile]
 
+
 def wordsOfLen(num):
     return list(filter(lambda x: len(x) == num, words))
+
 
 maxLength = 6
 minLength = 3
@@ -24,14 +28,16 @@ maxLengthWord = random.choice(maxLengthWords)
 letters = scramble(maxLengthWord)
 lettersCounter = Counter(letters)
 
+
 def isAnagram(word):
     wordCounter = Counter(word)
     return [wordCounter[letter] <= lettersCounter[letter] for letter in wordCounter].count(False) == 0
 
+
 validWords = dict()
 for i in range(minLength, maxLength + 1):
     iLength = list(filter(isAnagram, wordsOfLen(i)))
-    if(len(iLength) != 0):
+    if len(iLength) != 0:
         validWords[i] = iLength
 
 lengthsGuessed = dict()
@@ -40,12 +46,12 @@ for length in validWords.keys():
 
 startTime = time.time()
 checkMark = u'\u2713'
-while(list(lengthsGuessed.values()).count(False) != 0):
+while list(lengthsGuessed.values()).count(False) != 0:
     print(' '.join(letters))
     [print(f"{i} {checkMark if lengthsGuessed[i] else 'x'}    ", end="") for i in lengthsGuessed]
     print()
     guess = input("Guess a word: ")
-    if(len(guess) in validWords.keys() and guess in validWords[len(guess)]):
+    if len(guess) in validWords.keys() and guess in validWords[len(guess)]:
         print(u'\u2713')
         lengthsGuessed[len(guess)] = True
     else:
